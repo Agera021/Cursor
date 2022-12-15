ff985aa00299e7fd4fbe3f48e0ae99d51cffb281
Ager <agera21white@gmail.com>
2022-12-15 21:13:51 +0200

    initial commit

---
+++ HW2/script.js
@@ -0,0 +1,17 @@
+let N = 0;
+let M = 0;
+let even_numbers;
+let result = 0;
+
+do { !Number.isInteger(N)||!Number.isInteger(M)? alert("Це не Ваш вік") : N=0, M=0;
+N = Number(prompt("Введіть свій вік"))
+M = Number(prompt("Добре, це була гарна спроба, але тепер введіть свій справжній вік"))
+even_numbers = confirm("Пропускаємо парні?");
+} while (!Number.isInteger(N)||!Number.isInteger(M));
+
+for (let i = N; i < (M+1); i++) {
+    if (even_numbers){i%2 ? result += i : result;}
+    else {result += i;}
+};
+console.log(result);
+document.writeln ("Відповідь = " , result);
