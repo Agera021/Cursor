6baaef941ae493608833b93145881c165421e2af
Ager <agera21white@gmail.com>
2022-12-24 12:05:10 +0200

    homework2

--- HW2/script.js
+++ HW2/script.js
@@ -1,17 +1,38 @@
+const wrap = document.querySelector(".bmw");
+
 let N = 0;
 let M = 0;
-let even_numbers;
+
+
+do { N = Number(prompt("Введіть свій вік"));
+if (!Number.isInteger(N)|| N <= 0) {
+    alert("Це не Ваш вік")
+}
+}
+while (!Number.isInteger(N)|| N <= 0);
+
+
+do {
+    M = Number(prompt("Добре, це була гарна спроба, але тепер введіть свій справжній вік"));
+    if (!Number.isInteger(N)|| M <= N) {
+        alert("Це не Ваш вік")
+    }
+}
+while (!Number.isInteger(N)|| M <= N)
+
+const even_numbers = confirm ("Пропускати парні?");
 let result = 0;
 
-do { N>M || !Number.isInteger(N)||!Number.isInteger(M)? alert("Це не Ваш вік") : N=0, M=0;
-N = Number(prompt("Введіть свій вік"))
-M = Number(prompt("Добре, це була гарна спроба, але тепер введіть свій справжній вік"))
-even_numbers = confirm("Пропускаємо парні?");
-} while (N>M ||!Number.isInteger(N)||!Number.isInteger(M));
-
-for (let i = N; i < M; i++) {
-    if (even_numbers){i%2 ? result += i : result;}
-    else {result += i;}
-};
-console.log(result);
-document.writeln ("Відповідь = " , result);
+for (let i = N; i <= M; i++) {
+    if (even_numbers === true && i % 2 !== 0) {
+    result += i 
+    wrap.innerHTML = `Відповідь: ${result}`;
+    console.log(i);
+} else if (even_numbers === false) {
+  result += i;
+  wrap.innerHTML = `Відповідь: ${result}`;
+}
+else {
+    continue;
+}
+}
