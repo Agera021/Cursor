2a9375a62a4876bd7d50d49aa9d9afd27471bc2e
Ager <agera21white@gmail.com>
2022-12-24 12:51:27 +0200

    homework3

--- HW3/script.js
+++ HW3/script.js
@@ -17,19 +17,31 @@ function getDegree(number, numbersDegree) {
     return NumberOfDegree;
 };
 
-function correctName(wrongName) {
-    return wrongName[0].toUpperCase() + wrongName.slice(1).toLowerCase();
-};
+function getNameFirstLetterBig (name){
+	name = name.toLowerCase().split('');
+  const bigLetter = (name[0].toUpperCase());
+	let eachLetter = '';
+	for (let i = 0; i < (name.length) - 1; i++){
+		eachLetter += (name[i + 1] + ''); 
+    }
+  const result = (bigLetter + eachLetter);
+  return result;	
+}
 
-function salary(salaryWithTaxes, taxes) {
-    return salaryWithTaxes - (salaryWithTaxes * (taxes / 100));
-};
+function getSaleryNetto (salery) {
+	const firstTax = 18;
+	const secondTax = 1.5;
+	const taxes = (firstTax + secondTax);
+	const sumOfTaxes = (salery * taxes / 100);
+	const saleryNetto = (salery - sumOfTaxes);
+	return saleryNetto;
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
-function countLetter(leter, word) {
+function getCountLetter (leter, word) {
     let quantityOfLetters = 0;
     for (let i = 0; i <= word.length; i++) {
         if (word[i] == leter) {
@@ -39,19 +51,21 @@ function countLetter(leter, word) {
     return quantityOfLetters;
 };
 
-function convertCurrency(money) {
-    let result = "";
-    if (money.toUpperCase().indexOf("UAH") >= 0) {
-        result = money.toUpperCase().replace("UAH", "") / 40 + " $";
-    } else if (money.indexOf("$") >= 0) {
-        result = money.replace("$", "") * 40 + " UAH";
-    } else {
-        result = "We can't change your money";
-    }
-    return result;
-};
+function convertCurency (curency){
+	const dollarsBe = 40.60;
+	const uahBe = 0.036; 
+	let result = 0;
+	if (curency.includes('$')){
+		result = (parseInt(curency) * dollarsBe + 'UAH');
+	} else if (curency.includes('UAH')) {
+		result = (parseInt(curency) * uahBe + '$');
+	} else{
+		throw ('Error');
+	}
+	return result;
+	}
 
-function getRandomPassword(passwordLength = 8) {
+function getRandomPassword(passwordLength = 7) {
     let password = "";
     for (let i = 0; i < passwordLength; i++) {
         password += Math.floor(Math.random() * 10);
@@ -59,17 +73,17 @@ function getRandomPassword(passwordLength = 8) {
     return password;
 };
 
-function deleteLetters(a, word) {
-    result = word.replaceAll(a, '');
+function deleteLetters(e, word) {
+    result = word.replaceAll(e, '');
     return result
 };
 
 document.writeln(`Функція 1: ${getMaxDigit("1234567965")}`,  "<br />");
 document.writeln(`Функція 2: ${getDegree(1024, 4)}`, "<br />");
-document.writeln(`Функція 3: ${correctName("koeniGgsEg")}`, "<br />");
-document.writeln(`Функція 4: ${salary(1000, 19.5)}`, "<br />");
+document.writeln(`Функція 3: ${getNameFirstLetterBig("koeniGgsEg")}`, "<br />");
+document.writeln(`Функція 4: ${getSaleryNetto(1000, 19.5)}`, "<br />");
 document.writeln(`Функція 5: ${getRandomInt(1, 10)}`, "<br />");
-document.writeln(`Функція 6: ${countLetter("a", "batat")}`,"<br />");
-document.writeln(`Функція 7-8: ${convertCurrency("100$")}`, "<br />");
+document.writeln(`Функція 6: ${getCountLetter("a", "batat")}`,"<br />");
+document.writeln(`Функція 7-8: ${convertCurency("1000$")}`, "<br />");
 document.writeln(`Функція 9-10: ${getRandomPassword(7)}`, "<br />");
-document.writeln(`Функція 11: ${deleteLetters("a", "batat")}`,"<br />");
+document.writeln(`Функція 11: ${deleteLetters("e", "koeniggseg agera regera")}`,"<br />");
