b19fb106bce9227a341c9fa5af446cac66098cd8
Ager <agera21white@gmail.com>
2023-01-22 10:53:40 +0200

    homework11

---
+++ HW11/script.js
@@ -0,0 +1,24 @@
+function getChar() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const sign = Date.now()
+          .toString()
+          .slice(-5);
+        resolve(String.fromCharCode(sign));
+      }, 50);
+    });
+  }
+  
+  async function getRandomChinese(length) {
+    let str = "",
+      i = 1;
+    while (i <= length) {
+      str += await getChar();
+      i++;
+    }
+    return str;
+  }
+  
+  getRandomChinese(4).then(result => {
+    console.log(result);
+  });
